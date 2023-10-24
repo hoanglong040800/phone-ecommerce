8abff4c805266fa6f2d0e29e3ce7031b0de0d062
hoanglong040800 <hoanglong040800@gmail.com>
2023-10-24 23:00:09 +0700

    feat: ng-src, ng-click

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,6 +1,13 @@
+const mapStateHungryPng = {
+  hungry:
+    "https://www.shutterstock.com/shutterstock/photos/1178357125/display_1500/stock-vector-vector-illustration-girl-holding-stomach-because-of-hungry-kid-thinking-of-food-and-holding-her-1178357125.jpg",
+  full: "https://www.shutterstock.com/image-vector/walking-full-stomach-man-cartoon-260nw-362321831.jpg",
+};
+
 const AppController = ($scope, $filter) => {
   $scope.name = "";
   $scope.totalValue = 0;
+  $scope.stateOfHungry = mapStateHungryPng.hungry
 
   $scope.displayNumeric = () => {
     $scope.totalValue = calculateNumericOfString($scope.name);
@@ -10,9 +17,13 @@ const AppController = ($scope, $filter) => {
     $scope.name = $filter("uppercase")($scope.name);
   };
 
+  $scope.sayMessage = () => "My dog is so cute";
+
+  $scope.feedMe = () => {
+    $scope.stateOfHungry = mapStateHungryPng.full;
+  };
+
   const calculateNumericOfString = (string) => string.length;
 };
-
-AppController.$inject = ["$scope", "$filter"];
 
 angular.module("myApp").controller("appController", AppController);
