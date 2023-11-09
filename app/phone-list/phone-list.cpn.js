329db12d7a5bba80045fa0a1e7560d36c3f18a90
hoanglong040800 <hoanglong040800@gmail.com>
2023-11-09 10:25:41 +0700

    feat: order via dropdown

--- app/phone-list/phone-list.cpn.js
+++ app/phone-list/phone-list.cpn.js
@@ -9,14 +9,19 @@ function controller() {
     {
       name: "Xiaomi",
       desc: "Xiaomi phones are popular for their affordability and impressive features. They offer great value for your money, with sleek designs and powerful hardware. For example, the Xiaomi Mi 11 boasts a high-quality camera, fast processor, and a stunning display, all at a reasonable price.",
+      id: 1,
     },
     {
       name: "Iphone",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur at eaque voluptatum minima autem, minus quaerat pariatur reprehenderit nobis delectus?",
+      id: 2,
     },
     {
       name: "Samsung",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique id assumenda dolorum repellat sit sint accusantium fugit, natus quasi minus ipsam dolore voluptas omnis tempora?",
+      id: 3,
     },
   ];
+
+  this.sortCategory = "id";
 }
