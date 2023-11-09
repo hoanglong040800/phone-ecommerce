008074a104124fbd41af8279cc7e4657069fdba1
hoanglong040800 <hoanglong040800@gmail.com>
2023-11-09 10:44:35 +0700

    feat: fetch data via api

--- app/phone-list/phone-list.cpn.js
+++ app/phone-list/phone-list.cpn.js
@@ -1,27 +1,13 @@
 angular.module("phoneList").component("phoneList", {
   // scope is /app folder. Therefore need to import like at /app
   templateUrl: "phone-list/phone-list.html",
-  controller,
+  controller: ['$http', controller]
 });
 
-function controller() {
-  this.phones = [
-    {
-      name: "Xiaomi",
-      desc: "Xiaomi phones are popular for their affordability and impressive features. They offer great value for your money, with sleek designs and powerful hardware. For example, the Xiaomi Mi 11 boasts a high-quality camera, fast processor, and a stunning display, all at a reasonable price.",
-      id: 1,
-    },
-    {
-      name: "Iphone",
-      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur at eaque voluptatum minima autem, minus quaerat pariatur reprehenderit nobis delectus?",
-      id: 2,
-    },
-    {
-      name: "Samsung",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique id assumenda dolorum repellat sit sint accusantium fugit, natus quasi minus ipsam dolore voluptas omnis tempora?",
-      id: 3,
-    },
-  ];
-
+function controller($http) {
   this.sortCategory = "id";
+
+  $http.get("https://dummyjson.com/products").then((res) => {
+    this.phones = res.data.products;
+  });
 }
