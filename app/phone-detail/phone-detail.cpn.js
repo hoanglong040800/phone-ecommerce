45407dbfa22bf6b5bc77e7f1b5e78b51b6be29aa
hoanglong040800 <hoanglong040800@gmail.com>
2023-11-09 11:29:33 +0700

    feat: phone detail ui

--- app/phone-detail/phone-detail.cpn.js
+++ app/phone-detail/phone-detail.cpn.js
@@ -1,8 +1,14 @@
 angular.module("phoneDetail").component("phoneDetail", {
   templateUrl: "phone-detail/phone-detail.html",
-  controller: ["$routeParams", controller],
+  controller: ["$routeParams", "$http", controller],
 });
 
-function controller($routeParams) {
+function controller($routeParams, $http) {
   this.phoneId = $routeParams.phoneId;
+
+  $http
+    .get(`https://dummyjson.com/product/${this.phoneId}`)
+    .then(({ data }) => {
+      this.phone = data;
+    });
 }
